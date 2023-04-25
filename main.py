6f0ad8c41325253feacac3c8f2ed6c691d50be85
Obedaya <yeneuro.mail@gmail.com>
2023-04-25 16:13:34 +0200

    Added Model class and first functions

---
+++ main.py
@@ -0,0 +1,14 @@
+class Model:
+    def create_field(self):
+        return [ [0]*3 for i in range(3)]
+
+    def make_move(self, move, field, player):
+        x, y = move
+        if player == "x":
+            field[x][y] = 1
+        elif player == "o":
+            field[x][y] = 0
+
+
+test = Model()
+print(test.create_field())
