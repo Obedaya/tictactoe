28f8eef5770cb8790918b68f5abf2781ae7d1442
sh29155 <simon.holzner@stud.th-deg.de>
2023-04-26 17:22:18 +0200

    Started is_win and valid move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 class Model:
     def create_field(self):
-        return [ [0]*3 for i in range(3)]
+        return [[0] * 3 for i in range(3)]
 
     def make_move(self, move, field, player):
         x, y = move
@@ -9,8 +9,22 @@ class Model:
         elif player == "o":
             field[x][y] = 0
 
+    def is_valid_move(self, field, move):
+        x, y = move
+        if field[x][y] == 1 or field[x][y] == 0:
+            return False
+        else:
+            return True
+
+    def is_win(self, field, player):
+        for i in range(3):
+            if all(x == field[i][0] for x in field):
+                return field[i][0]
+        for i in range(3):
+            if all(x == field[0][i] for x in field):
+                return field[0][i]
+
 
 test = Model()
 print(test.create_field())
 print("Hello World")
-
